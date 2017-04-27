9464bd021afa073e7442e50fd17c4602a1e668a0
Wiebe Cnossen <wiebe.cnossen@infoprojects.nl>
2017-04-27 23:09:07 +0200

    if let replacing match

--- src/algorithm/alphabeta.rs
+++ src/algorithm/alphabeta.rs
@@ -18,47 +18,42 @@ pub fn makes_cut<TGame, TScope>(judge: &mut Judge, metric: &mut Metric, position
   let quiet = judge.quiet_position(position, &moves);
   let current_score = judge.evaluate(position);
 
-  match scope.next(quiet, cut - current_score) {
-    None => SearchResult::evaluation(current_score),
-    Some(_) => {
-      let mut best = MIN_EVAL;
-      let mut pending = None;
-      for mv in moves {
-        let quiet = judge.quiet_move(position, &mv);
-        let score =
-          match scope.next(quiet, cut - current_score) {
-            None => current_score,
-            Some(next) => {
-              -makes_cut(
-                judge,
-                metric,
-                &position.go(&mv),
-                &next,
-                -cut+1).evaluation
-            }
-          };
-        if score > best {
-          best = score;
-          pending = Some(mv);
-          if best >= cut { break; }
-        }
-      }
-
-      if best >= cut {
-        if let Some(mv) = pending {
-          judge.remember(position, scope.depth(), best, mv, false);
-          SearchResult::with_move(mv, best)
+  if let Some(_) = scope.next(quiet, cut - current_score) {
+    let mut best = MIN_EVAL;
+    let mut pending = None;
+    for mv in moves {
+      let quiet = judge.quiet_move(position, &mv);
+      let score =
+        if let Some(next) = scope.next(quiet, cut - current_score) {
+          -makes_cut(judge, metric, &position.go(&mv), &next, -cut + 1).evaluation
         }
         else {
-          panic!("No move pending");
-        }
-      }
-      else {
-        if let Some(mv) = pending {
-          judge.remember(position, scope.depth(), best, mv, true);
-        }
-        SearchResult::evaluation(best)
+          current_score
+        };
+      if score > best {
+        best = score;
+        pending = Some(mv);
+        if best >= cut { break; }
       }
     }
+
+    if best >= cut {
+      if let Some(mv) = pending {
+        judge.remember(position, scope.depth(), best, mv, false);
+        SearchResult::with_move(mv, best)
+      }
+      else {
+        panic!("No move pending");
+      }
+    }
+    else {
+      if let Some(mv) = pending {
+        judge.remember(position, scope.depth(), best, mv, true);
+      }
+      SearchResult::evaluation(best)
+    }
+  }
+  else {
+      SearchResult::evaluation(current_score)
   }
 }
