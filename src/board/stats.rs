db6195becfdcde8c8bf774f7148cd27ae8d71e18
Wiebe Cnossen <wiebe.cnossen@gmail.com>
2017-09-06 19:50:32 +0200

    height added to stats

--- src/board/stats.rs
+++ src/board/stats.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 use algorithm::judge::Eval;
 use board::piece::{WHITE_MAN, BLACK_MAN};
 use board::position::Position;
@@ -8,6 +10,8 @@ pub struct PositionStats {
     pub voffset_black: [Eval; 10],
     pub hoffset_white: [Eval; 10],
     pub hoffset_black: [Eval; 10],
+    pub height_white: usize,
+    pub height_black: usize,
 }
 
 impl PositionStats {
@@ -17,6 +21,10 @@ impl PositionStats {
         let mut voffset_black = [0; 10];
         let mut hoffset_white = [0; 10];
         let mut hoffset_black = [0; 10];
+        let mut vmin_white = 9;
+        let mut vmax_white = 0;
+        let mut vmin_black = 9;
+        let mut vmax_black = 0;
 
         for field in 0..50 {
             let piece = position.piece_at(field);
@@ -27,23 +35,37 @@ impl PositionStats {
                     hoffset_white[x] += 1;
                     let y = 9 - field / 5;
                     voffset_white[y] += 1;
+                    vmin_white = min(vmin_white, y);
+                    vmax_white = max(vmax_white, y);
                 }
                 BLACK_MAN => {
                     let x = 8 - 2 * (field % 5) + field / 5 % 2;
                     hoffset_black[x] += 1;
                     let y = field / 5;
                     voffset_black[y] += 1;
+                    vmin_black = min(vmin_black, y);
+                    vmax_black = max(vmax_black, y);
                 }
                 _ => (),
             };
         }
 
         PositionStats {
-            piece_count: piece_count,
-            voffset_white: voffset_white,
-            voffset_black: voffset_black,
-            hoffset_white: hoffset_white,
-            hoffset_black: hoffset_black,
+            piece_count,
+            voffset_white,
+            voffset_black,
+            hoffset_white,
+            hoffset_black,
+            height_white: if vmax_white > vmin_white {
+                vmax_white - vmin_white
+            } else {
+                0
+            },
+            height_black: if vmax_black > vmin_black {
+                vmax_black - vmin_black
+            } else {
+                0
+            },
         }
     }
 }
